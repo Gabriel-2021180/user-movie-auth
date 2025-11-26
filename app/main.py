c1f4d6dd739bb6629fb3a3e3c6286b4bffae1d5e
Gabriel-2021180 <azurduy10082004@gmail.com>
2025-11-26 15:36:47 -0400

    cors para aceptar peticiones de todo lado

--- app/main.py
+++ app/main.py
@@ -1,42 +1,45 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from app.db.session import create_db_and_tables
 from app.api.v1.endpoints import auth, favorites
-
-# --- IMPORTS NUEVOS PARA RATE LIMIT ---
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from app.core.limiter import limiter
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    create_db_and_tables()
+    # create_db_and_tables() # Descomentar si necesitas inicializar tablas
     print("------> ¡SERVIDOR LISTO! <------")
     yield
 
 app = FastAPI(
     title="Movie Identity Service",
+    version="1.0.0",
     lifespan=lifespan
 )
 
-# --- CONECTAR LIMITADOR A LA APP ---
+# Conectar limitador de velocidad
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
-# CORS
-origins = ["http://localhost:3000", "http://127.0.0.1:3000"]
+# --- CONFIGURACIÓN CORS PERMISIVA (Para evitar dolores de cabeza) ---
+# Allow origins = ["*"] permite que CUALQUIER sitio web consuma tu API.
+# Esto es ideal para desarrollo y pruebas iniciales.
+# Cuando termines todo el proyecto, puedes cambiarlo por tu dominio real.
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+# Rutas
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(favorites.router, prefix="/api/v1/favorites", tags=["Favorites"])
 
 @app.get("/")
 def read_root():
-    return {"message": "API Activa"}
+    return {"status": "online", "message": "API Funcionando 🚀"}
